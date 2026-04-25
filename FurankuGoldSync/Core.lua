831a99d5f74df015bba7370484091d38db067e01
Furanku2908 <mail@furanku.de>
2026-04-25 22:58:13 +0200

    Add logging functions and event handling for login

--- FurankuGoldSync/Core.lua
+++ FurankuGoldSync/Core.lua
@@ -1,11 +1,29 @@
 local addonName, addon = ...
 
-local frame = CreateFrame("Frame")
+-- constants / yettings
+local PREFIX = "|cffA335EE[FGS]|r "
+local FULLNAME = "|cffA335EE[Furanku Gold Sync]|r "
 
+-- helper-functions
+local function Print(msg)
+    print(PREFIX .. tostring(msg))
+end
+local function NamePrint(msg)
+    print(FULLNAME .. tostring(msg))
+end
+
+
+-- main-function
+local function OnLogin()
+    Print("Loaded!")
+end
+
+-- register events
+local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_LOGIN")
 
 frame:SetScript("OnEvent", function(self, event)
     if event == "PLAYER_LOGIN" then
-        print("Furanku Gold Sync loaded!")
+        OnLogin()
     end
 end)
